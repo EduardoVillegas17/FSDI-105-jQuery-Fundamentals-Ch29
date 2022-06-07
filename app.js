7ad1302e9a782e2efe0fd1a3b09f3cbeef357566
Villegas Ramirez Luis Eduardo <60408295+EduardoVillegas17@users.noreply.github.com>
2022-06-07 16:17:24 -0700

    Session#2
    - Save the users in the localStorage.
    
    - Add css to improve the presentation of the application
    
    - Create a NavBar using your own CSS and put the NavBar on every html page on the system.
    
    a) link the index to the register.html (sing up).
    
    b) link the register.html to the index.html.
    
    c) link the user.html to the index.html and the register.html
    
    - Clear inputs functions using jQuery.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //object constructor
-function User(email,password,fname,lname,age,address,pnumber,payment,color){
+function User(email,password,firstName,lastName,age,address,pnumber,payment,color){
     this.email=email;
     this.password=password;
-    this.firstName=fname;
-    this.lastName=lname;
+    this.firstName=firstName;
+    this.lastName=lastName;
     this.age=age;
     this.address=address;
     this.pnumber=pnumber;
@@ -11,6 +11,55 @@ function User(email,password,fname,lname,age,address,pnumber,payment,color){
     this.color=color;
 
 
+}
+function isValid(user){
+    let valid=true;
+    if(user.email.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.password.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.firstName.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.lastName.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.age.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.pnumber.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.payment.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.color.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    if(user.address.length==0){
+        valid=false;
+        alert("please complete all fields");
+        $("input").addClass("input-error");
+    }
+    return valid;
 }
 //register function
 function register(){
@@ -28,15 +77,23 @@ function register(){
     console.log(userName,userPass,userFirstName,userLastName,userAge,userAddress,userPhoneNumber,userPayment,userColor);
 
     //creatig the obj
-    let newUser = User(userName,userPass,userFirstName,userLastName,userAge,userAddress,userPhoneNumber,userPayment,userColor);
+    let newUser = new User(userName,userPass,userFirstName,userLastName,userAge,userAddress,userPhoneNumber,userPayment,userColor);
+    if(isValid(newUser)==true){
+        //console.log(newUser);
+        saveUser(newUser);//this function  is in the StoreManager
     //clear the inputs
-    $(`input`).val("");
-    console.log(newUser);
+    $("input").val("");
+    }
 }
 function init(){
-    console.log("register");
+    console.log("Register");
     //hook events
     $("#btnRegister").click(register);
+    $("#txtLastName").keypress(function(e){
+        if(e.key=="Enter"){
+            register();
+        }
+    })
 
 }
 window.onload=init;
